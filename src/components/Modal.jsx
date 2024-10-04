82eefd1c6a86903e047e8b807fe717b896ef0a89
anouar garbaa <g.anouar@yahoo.com>
2024-10-04 17:03:26 +0100

    add an error dialog

---
+++ src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <form method="dialog">
+        <button>{buttonCaption}</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal-root")
+  );
+});
+
+export default Modal;
+
+/**
+ * to expose a function that can be called from outside this component function, we must use useImperativeHandle()
+ */
