c5c548703d742a8b525126f0385cef965c0f34e4
anouar garbaa <g.anouar@yahoo.com>
2024-10-04 15:02:26 +0100

    handling project creation

--- src/App.jsx
+++ src/App.jsx
@@ -20,32 +20,33 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
 
       return {
         ...prevState,
-        projects: [...prevState.projects, newProject]
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
       };
     });
   }
 
-  console.log(projectsState)
-
   let content;
   if (projectsState.selectedProjectId === null) {
-
     content = <NewProject onAdd={handleAddProject} />;
-    
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
